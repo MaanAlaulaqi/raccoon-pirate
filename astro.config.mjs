89f314cff273f3a0d975cbb43d2d64ad480ba478
MaanAlaulaqi <thema3an@gmail.com>
2025-07-12 17:10:24 +0400

    bleh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', 
-  adapter: vercel({}),
+  adapter: vercel(),
   server: {
     port: 9001,
   },
-
 });
